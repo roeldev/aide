3eb52001392d85b9384dc79067afcf04a7e6f95c
Roel Dev <roeldev@users.noreply.github.com>
2015-10-26 10:16:18 +0100

    Added browserify and babelify

--- src/api-classlist.js
+++ src/api-classlist.js
@@ -73,15 +73,15 @@ var API = function($flag)
  * Set a global class flag to the document <html> root element.
  *
  * @param {string} $flag
- * @param {string} $value
- * @param {boolean} $boolean
+ * @param {string} $value ['']
+ * @param {boolean} $bool [true]
  * @return {boolean}
  */
-API.set = function($flag, $value='', $boolean=true)
+API.set = function($flag, $value='', $bool=true)
 {
     // it is possible to conditionally set or unset a flag/value. when the
-    // $boolean param is false, unset the (already added) flag.
-    if ($boolean === false)
+    // $bool param is false, unset the (already added) flag.
+    if ($bool === false)
     {
         return API.unset($flag);
     }
@@ -158,4 +158,4 @@ API.toggle = function($flag)
     return $result;
 };
 
-export default API;
+module.exports = API;
