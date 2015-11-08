18b17ba7a32a9ed7da3855b3d3778ee93c07f6c4
Roel Dev <roeldev@users.noreply.github.com>
2015-11-08 14:52:14 +0100

    Changes to classList version of Aide.State API

--- src/state.js
+++ src/state.js
@@ -1,3 +1,21 @@
-module.exports = (document.documentElement.classList ?
-                  require('./state/api-classlist.js') :
-                  require('./state/api-regexp.js'));
+'use strict';
+
+var API = (document.documentElement.classList ?
+           require('./state/classlist.js') :
+           require('./state/regexp.js'));
+
+/**
+ * A reference to the <html> element in the document root.
+ * @type {HTMLElement}
+ */
+API.TARGET = document.documentElement;
+
+/**
+ * The seperator sequence between a flag and a value (eg. flag--value).
+ * @type {string}
+ */
+API.SEPERATOR = '--';
+
+// -----------------------------------------------------------------------------
+
+module.exports = API;
