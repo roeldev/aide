9263ba66af9cd20d1b400ece54f191f6ab88275e
Roel Dev <roeldev@users.noreply.github.com>
2015-11-24 15:36:06 +0100

    Updated tests

--- test/state_regexp_tests.js
+++ test/state_regexp_tests.js
@@ -2,24 +2,27 @@
 
 describe('aide.state._regExp.search()', function aideSateRegExpSearchTests()
 {
-    var $target = document.createElement('div');
-    $target.className = 'foo test1 bar test2--value baz';
-
-    it('should return the full class when found', function()
+    beforeEach(function()
     {
-        var $actual = aide.state._regExp.search($target, 'test1');
+        this.target = document.createElement('div');
+        this.target.className = 'foo test1 bar test2--value baz';
+    });
+
+    it('return the full class when found', function()
+    {
+        var $actual = aide.state._regExp.search(this.target, 'test1');
         expect($actual).to.equal('test1');
     });
 
-    it('should return the full class + value when found', function()
+    it('return the full class + value when found', function()
     {
-        var $actual = aide.state._regExp.search($target, 'test2');
+        var $actual = aide.state._regExp.search(this.target, 'test2');
         expect($actual).to.equal('test2--value');
     });
 
-    it('should return false when not found', function()
+    it('return false when not found', function()
     {
-        var $actual = aide.state._regExp.search($target, 'not-set');
+        var $actual = aide.state._regExp.search(this.target, 'not-set');
         expect($actual).to.equal(false);
     });
 });
--- test/state_tests.js
+++ test/state_tests.js
@@ -1,30 +1,30 @@
-var Target = document.documentElement;
-
-// -----------------------------------------------------------------------------
-
 describe('aide.state()', function aideSateTests()
 {
-    Target.className = 'foo test1 bar test2--value baz';
+    beforeEach(function()
+    {
+        this.target = document.documentElement;
+        this.target.className = 'foo test1 bar test2--value baz';
+    });
 
-    it('should return true when the flag is set', function()
+    it('return true when the flag is set', function()
     {
         var $actual = aide.state('test1');
         expect($actual).to.equal(true);
     });
 
-    it('should return false when the flag is not set', function()
+    it('return false when the flag is not set', function()
     {
         var $actual = aide.state('not-set');
         expect($actual).to.equal(false);
     });
 
-    it('should return true when the flag is expected to be not set', function()
+    it('return true when the flag is expected to be not set', function()
     {
         var $actual = aide.state('not-set', false);
         expect($actual).to.equal(true);
     });
 
-    it('should return true when the flag has the expected value', function()
+    it('return true when the flag has the expected value', function()
     {
         var $actual = aide.state('test2', 'value');
         expect($actual).to.equal(true);
