521ddd24524bc1ae7bc3d0da488a746919702a6a
Roel Dev <roeldev@users.noreply.github.com>
2015-12-11 10:37:11 +0100

    Version bump

--- dist/aide-state.js
+++ dist/aide-state.js
@@ -1,4 +1,4 @@
-/*! aide.state v0.1.0 | GPL-2.0+ License | (c) 2014-2015 Roel Schut (http://roelschut.nl) | https://github.com/roeldev/aide */
+/*! aide.state v0.1.1 | GPL-2.0+ License | (c) 2014-2015 Roel Schut (http://roelschut.nl) | https://github.com/roeldev/aide */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.aide || (g.aide = {})).state = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * aide | src/state.js
@@ -119,7 +119,7 @@ API.unset = function ($flag) {
     var $result = false;
     var $found = API.search($flag);
 
-    if ($found !== false && $flag != $found) {
+    if ($found !== false) {
         Utils.remove(API.TARGET, $found);
         $result = true;
     }
@@ -131,16 +131,19 @@ API.unset = function ($flag) {
  * already set. Remove it when it is.
  *
  * @param {string} $flag
+ * @param {string} $value ['']
  * @return {boolean}
  */
 API.toggle = function ($flag) {
+    var $value = arguments.length <= 1 || arguments[1] === undefined ? '' : arguments[1];
+
     var $result = false;
     var $found = API.search($flag);
 
     if ($found !== false) {
         Utils.remove(API.TARGET, $found);
     } else {
-        Utils.add(API.TARGET, $flag);
+        API.set($flag, $value);
         $result = true;
     }
 
